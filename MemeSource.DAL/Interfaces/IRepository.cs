ba92a62eadb2d5577de8cdc80d114ae08014fcb6
Andy <60883641+andy832255@users.noreply.github.com>
2024-08-02 00:16:39 +0800

    Add IRepository、IUnitOfWork

---
+++ MemeSource.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq.Expressions;
+
+namespace MemeSource.DAL.Interfaces
+{
+    /// <summary>
+    /// 泛型Repository
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IRepository<T> where T : class
+    {
+        /// <summary>
+        /// 取得全部
+        /// </summary>
+        /// <param name="filter">查詢條件</param>
+        /// <param name="includeProperties"></param>
+        /// <returns></returns>
+        Task<ICollection<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
+        /// <summary>
+        /// 取得單筆
+        /// </summary>
+        /// <param name="filter">查詢條件</param>
+        /// <param name="includeProperties"></param>
+        /// <returns></returns>
+        Task<T> GetAsync(Expression<Func<T, bool>> filter, string? includeProperties = null);
+        void Add(T entity);
+        void Remove(T entity);
+        //void RemoveRange(IEnumerable<T> entity);
+        void Update(T entity);
+    }
+}
---
+++ MemeSource.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MemeSource.DAL.Interfaces
+{
+    /// <summary>
+    /// UnitOfWork interface
+    /// </summary>
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>
+        /// DB Context
+        /// </summary>
+        DbContext Context { get; }
+
+        /// <summary>
+        /// Saves the change
+        /// </summary>
+        /// <returns></returns>
+        Task<int> SaveChangeAsync();
+    }
+}
